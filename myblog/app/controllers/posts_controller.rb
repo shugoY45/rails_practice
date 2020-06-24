3af06913e5dbb4c9f1811789f8f2384d4e0109c8
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-06-24 16:11:05 +0900

    #13 画像にリンクを貼ってみよう

---
+++ myblog/app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+
+  #Convention over Configuration (CoC)
+  def index
+    @posts = Post.all.order(created_at: 'desc')
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+end
